38df7272bf2bedf05c6cf9bcd01f00dcbedf1545
n3sh-source <gustavo.carakaki@gmail.com>
2021-04-16 23:19:57 -0300

    Add css and script

--- http/server.js
+++ http/server.js
@@ -1,10 +1,3 @@
-/*
-http.createserver((req,res) => {
-    res.write('Hi')
-    res.end()
-}).listen(5000)
-*/
-
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
@@ -12,20 +5,20 @@ const path = require('path');
 http.createServer((req, res) => {
 
     const file = req.url == '/' ? 'index.html' : req.url;
-    console.log(file);
+    const filePath = path.join(__dirname,'public', file);
 
-    // if(req.url == '/'){
-    //     fs.readFile(
-    //         path.join(__dirname, 'public', 'index.html'),
-    //         (err, content) => {
-    //             if(err) throw err;
+    const extname = path.extname(filePath);
+    const AllowedFileTypes = ['.html', '.css', '.js'];
+    const allowed = AllowedFileTypes.find(item => {item == extname});
 
-    //             res.end(content)
-    //         }
-    //     );
-    // };
-        
-    
-    /*if(req.url == '/contato')
-        return res.end('<h1>Contato</h1>')*/
+    if(!allowed) return
+
+        fs.readFile(
+            filePath,
+            (err, content) => {
+                if(err) throw err;
+
+                res.end(content)
+            }
+        );
 }).listen(5000, () => console.log('Server On'));
